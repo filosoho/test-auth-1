4831889fe0c63e7753adac806771143786363bc0
Anna <filosoho@gmail.com>
2024-07-16 12:06:17 +0100

    Add an endpoint to fetch articles Task 5 CORE-GET/api/articles - additionally removes the queries.model.js and combins it with articles.model.js and topics.model.js, adds error handler for psql error code 22P02

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,15 @@
-const { fetchArticleById } = require("../models/articles.model.js");
+const {
+  fetchArticles,
+  fetchArticleById,
+} = require("../models/articles.model.js");
+
+exports.getArticles = (req, res, next) => {
+  fetchArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
+    })
+    .catch(next);
+};
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
--- models/articles.model.js
+++ models/articles.model.js
@@ -1,7 +1,27 @@
-const { selectArticleByIdQuery } = require("./queries.model.js");
+const db = require("../db/connection.js");
+
+exports.fetchArticles = () => {
+  const queryStr = `
+  SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url,
+  COUNT(comments.comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments ON comments.article_id = articles.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC;
+`;
+  return db
+    .query(queryStr)
+    .then(({ rows: articles }) => {
+      return articles;
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
 
 exports.fetchArticleById = (article_id) => {
   const articleId = Number(article_id);
+
   if (isNaN(articleId)) {
     return Promise.reject({
       status: 400,
@@ -9,8 +29,11 @@ exports.fetchArticleById = (article_id) => {
     });
   }
 
-  return selectArticleByIdQuery(articleId)
-    .then((article) => {
+  const queryStr = "SELECT * FROM articles WHERE article_id = $1;";
+  return db
+    .query(queryStr, [articleId])
+    .then(({ rows }) => {
+      const article = rows[0];
       if (!article) {
         return Promise.reject({
           status: 404,
